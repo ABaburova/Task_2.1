0613b8d2ec0be02110e5c58b3ad4cd8dbb916a0e
Anastasia Baburova <nas3235@yandex.ru>
2024-08-09 23:51:34 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13_676; //цена авиабилета
+        int bonus = price / 20; //формула расчета бонуса
+        System.out.print("Бонус = " + bonus);
+
+    }
+}
